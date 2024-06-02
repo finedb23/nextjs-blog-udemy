8b980a7a6b7c2dda7425fe28bbb28514ad87e6f2
finedb <finedb@gmail.com>
2024-06-02 11:28:47 +0900

    令和6年6月2日

---
+++ components/Layout.js
@@ -0,0 +1,49 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from './Layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+const name = 'Shin Code';
+import Image from 'next/image';
+
+export const siteTitle = 'Next.js ブログ';
+
+const Layout = ({ children, home }) => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
+              width={100}
+              height={100}
+              src="/images/profile.png"
+              alt=""
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+          <Image
+              className={`${utilStyles.borderCircle}`}
+              width={100}
+              height={100}
+              src="/images/profile.png"
+              alt=""
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        )}
+      </header>
+
+      <main>{children}</main>
+      {!home && (
+        <>
+            <Link href="/">ホームへ戻る</Link>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/500.js
@@ -0,0 +1,3 @@
+export default function Custom500() {
+    return <h1>サーバーが見つかりませんでした。</h1>
+}
